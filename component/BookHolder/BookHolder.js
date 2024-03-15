8d727bafc9b480386c30e5daca926847b20d33c6
JokerHDKing <2804183061@qq.com>
2024-03-15 14:24:15 +0800

    增加图书持有者展示

---
+++ component/BookHolder/BookHolder.js
@@ -0,0 +1,28 @@
+// component/BookHolder/BookHolder.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item:{
+      type:Object,
+      value:{
+        nickname:"默认昵称",
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
